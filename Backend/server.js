0f092f7e040be777b40e2911814b8ef1c6c9035f
tasveer-fatama <tasveerfatema135@gmail.com>
2024-11-18 07:37:00 +0530

    .....

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,15 @@
 const app = require("./app");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const config = require("./config/config.env")
 
 
 // config 
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config();
+console.log(process.env.PORT);
+
 
 app.listen(process.env.PORT,()=>{
-console.log(`server is running on http://localhost: ${process.env.PORT}`);
+console.log(`server is running on http://localhost:${process.env.PORT || 4000} `);
 
 
 })
