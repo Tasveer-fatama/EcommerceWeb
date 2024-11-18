a067d1558475f9accf108bffb9f29509659e178f
tasveer-fatama <tasveerfatema135@gmail.com>
2024-11-18 16:56:29 +0530

    ....

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -1,1 +1,13 @@
 const mongoose = require ("mongoose")
+
+const ConnectDb = ()=>{
+    mongoose.connect("mongodb://localhost:27017/EcommerceWeb").then((data)=>{
+        console.log(`Mongodb connected with server:${data.connection.host}`);
+        
+        }).catch((err)=>{
+        console.log(err);
+        
+        })
+}
+
+module.exports=ConnectDb
--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,22 @@
 const app = require("./app");
-const dotenv = require("dotenv");
-const config = require("./config/config.env")
+const dotenv = require('dotenv');
+const ConnectDB = require("./config/database")
+const cors = require("cors")
 
 
 // config 
-dotenv.config();
+dotenv.config({path:"backend/config/.env"});
 console.log(process.env.PORT);
+
+//enable cors
+app.use(cors());
+
+//connecting to database
+ConnectDB()
 
 
 app.listen(process.env.PORT,()=>{
-console.log(`server is running on http://localhost:${process.env.PORT || 4000} `);
+console.log(`server is running on http://localhost:${process.env.PORT || 4000} `)
 
 
 })
